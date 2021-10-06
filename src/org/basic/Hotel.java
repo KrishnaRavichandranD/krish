9eacb70d6232020cdabc3cb1744860de6aca753c
KrishnaRavichandranD <krishnaravi6494@gmail.com>
2021-10-06 21:11:17 +0530

    masss

---
+++ src/org/basic/Hotel.java
@@ -0,0 +1,37 @@
+package org.basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hotel {
+	public static void main(String[] args) {
+		
+	System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\mariv\\eclipse-workspace\\SeleniumTask1\\Driver\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("http://adactinhotelapp.com/");
+	
+	WebElement n = driver.findElement(By.id("username"));
+	
+	n.sendKeys("Krishna");
+	
+	WebElement p = driver.findElement(By.id("password"));
+
+	p.sendKeys("123456");
+	
+	
+
+	
+	
+		
+
+		
+		
+		
+	}
+
+}
